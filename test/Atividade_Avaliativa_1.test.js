6b2fcd4a32272b89e76c93c6dd943f609c70f879
Marcus Vinicus <mvbrazz@hotmail.com>
2021-11-06 17:51:13 -0300

    First commit

---
+++ test/Atividade_Avaliativa_1.test.js
@@ -0,0 +1,12 @@
+
+describe('Utils', () => {
+    describe('Test', () => {
+        test('Inicio', () => {
+
+            aux = 0;
+
+            expect(aux).toBe(0);
+        });
+ 
+    });    
+});
